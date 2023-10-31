2071c8986f0ad02c64b85b1fc175daa0f0f4312a
Demetrusa <demetrekurdadze20bgeolab.edu.ge>
2023-10-31 17:13:56 +0400

    feat:start new project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import './App.scss';
-import Button from './components/button/Button.jsx';
-import Navbar from './layout/Navbar.jsx';
-import Footer from './layout/footer/Footer';
-import Leagues from './layout/leagues/Leagues';
 
 function App() {
   return (
     <div className="App">
-      <div className='App__header'>
-        <Navbar navPd={true}/>
-        <div className='btn-pd'>
-          <Button />
-        </div>
-      </div>
-      <Leagues />
-      <Footer />
+
     </div>
   );
 }
